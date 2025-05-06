1581a32c7a2db791641cb7afc482d026b486601a
mtvento <mtvento@gmail.com>
2025-05-06 23:51:14 +0200

    Various fixes

---
+++ src/command/cuda_scaling_method.rs
@@ -0,0 +1,6 @@
+// src/cuda_scaling_method.rs
+
+pub fn apply_cuda_scaling_method(input: &str, output: &str) -> Result<(), String> {
+    // Implement logic to apply CUDA-based scaling methods
+    Ok(())
+}
